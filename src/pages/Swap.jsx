067d2841780b5ef8348b7f70520e18e005dc90ad
Cooldee0317 <cooldeelancer@gmail.com>
2024-07-19 15:06:16 +0800

    [changed] update UI

--- src/pages/Swap.jsx
+++ src/pages/Swap.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { IoIosArrowDown, IoMdSync } from 'react-icons/io'
+import ConnectButton from '../components/UI/ConnectButton'
 
 function Swap() {
   return (
-    <div className='flex justify-center items-center min-h-[calc(100vh-60px)] pb-[200px]'>
-      <div className='min-w-lg bg-slate-400 rounded-lg overflow-hidden shadow-lg px-5 py-5'>
+    <div className='flex justify-center items-center min-h-[calc(100vh-81px)] pb-[200px]'>
+      <div className='min-w-lg bg-white rounded-lg overflow-hidden shadow-lg px-5 py-5'>
         <div className='mb-2'>
           <div>
             <h2 className='font-bold text-[26px]'>RUNE SWAP</h2>
           </div>
         </div>
-        <div className='flex bg-slate-200 rounded-md '>
-          <div className=' px-5 py-3 border-r-2 border-gray-400'>
+        <div className='flex bg-[#f9faf7] border-[#e3e4e5] border-[0.5px] rounded-md '>
+          <div className=' px-5 py-3 border-r-[0.5px] border-[#e3e4e5]'>
             <div className='mb-2 mr-3 text-opacity-50'>
               <span>You pay</span>
             </div>
@@ -61,8 +62,8 @@ function Swap() {
         <div className='flex justify-center py-4'>
           <IoMdSync className='text-[36px]' />
         </div>
-        <div className='flex bg-slate-200 rounded-md mb-6'>
-          <div className=' px-5 py-3 border-r-2 border-gray-400'>
+        <div className='flex bg-[#f9faf7] border-[#e3e4e5] border-[0.5px] rounded-md mb-12'>
+          <div className=' px-5 py-3 border-r-[0.5px] border-[#e3e4e5]'>
             <div className='mb-2 mr-3 text-opacity-50'>
               <span>You receive</span>
             </div>
@@ -95,7 +96,7 @@ function Swap() {
             </div>
           </div>
         </div>
-          <button className='w-full bg-[#5c6879] py-2 rounded-md text-white font-bold hover:bg-[#252d37]'>Connect Wallet</button>
+          <button className='w-full bg-[#5c6879] py-2 rounded-md px-4 rounded-lg bg-black text-[#59B200]'>Connect Wallet</button>
         </div>
       </div>
   )
